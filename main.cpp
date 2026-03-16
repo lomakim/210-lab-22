8e347e981081800d538f4f10ea813d881de469fc
lomakim <lomakim@gmail.com>
2026-03-15 20:24:59 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 using namespace std;
 
-const int MIN_NR = 10,
-    MAX_NR = 99,
-    MIN_LS = 5,
-    MAX_LS = 20;
+const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
 
 class DoublyLinkedList {
     private: struct Node {
@@ -18,7 +15,6 @@ class DoublyLinkedList {
             next = n;
         }
     };
-
     Node * head;
     Node * tail;
 
@@ -78,33 +74,45 @@ class DoublyLinkedList {
         temp -> next = newNode;
     }
     
-    void delete_node(int value) {
-        if (!head) return; // Empty list
+    void delete_val(int value) {
+        if (!head) 
+            return; // Empty list
         Node * temp = head;
-        while (temp && temp -> data != value)
-            temp = temp -> next;
-        if (!temp) return; // Value not found
-        if (temp -> prev) {
-            temp -> prev -> next = temp -> next;
-        } else {
-            head = temp -> next; // Deleting the head
+        while (temp && (temp->data != value))
+            temp = temp->next;
+
+        if (!temp) 
+            return; // Value not found
+
+        if (temp->prev) {
+            temp->prev->next = temp->next; 
+        } 
+        else {
+            head = temp->next; // Deleting the head
         }
-        if (temp -> next) {
-            temp -> next -> prev = temp -> prev;
-        } else {
-            tail = temp -> prev; // Deleting the tail
+        if (temp->next) {
+            temp->next->prev = temp->prev;
+        } 
+        else {
+            tail = temp->prev; // Deleting the tail
         }
         delete temp;
     }
 
-    void delete_pos(Node* pos) {
-        Node * current = pos;
-        Node * successor = current->next;
-        Node * predecessor = current->prev;
+    void delete_pos(int pos) {
+        Node * current = head;
+        Node * successor = nullptr;
+        Node * predecessor = nullptr;
 
         if(!head) {
             cout << "List is empty!";
             return;
+        }
+
+        for(int i = 0; i < pos; i++) {
+            current = current->next;
+            successor = current->next;
+            predecessor = current->prev;
         }
 
         if (successor)
@@ -114,9 +122,33 @@ class DoublyLinkedList {
         if (current == head)
             head = successor;
         if (current == tail)
-            tail = predecessor
+            tail = predecessor;
 
         delete current;
+    }
+
+    void pop_front() {
+        if (!head) {
+            cout << "List is empty!";
+            return;
+        }
+        else {
+            Node * temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void pop_back() {
+        if (!head) {
+            cout << "List is empty!";
+            return;
+        }
+        else {
+            Node * temp = tail;
+            tail = tail->prev;
+            delete temp;
+        }
     }
 
     void print() {
@@ -150,16 +182,37 @@ class DoublyLinkedList {
 
 // Driver program
 int main() {
+    srand(time(0));
     DoublyLinkedList list;
-
     int size = rand() % (MAX_LS - MIN_LS + 1) + MIN_LS;
 
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < 16; ++i) {
         list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
+    }
     
     cout << "List forward: ";
     list.print();
+    cout << "List backward: ";
+    list.print_reverse();
 
+    cout << "Now deleting head using pop_front() function." << endl;
+    list.pop_front();
+
+    cout << "List forward: ";
+    list.print();
+    cout << "List backward: ";
+    list.print_reverse();
+
+    cout << "Now deleting tail using pop_back() fucntion." << endl;
+    cout << "List forward: ";
+    list.print();
+    cout << "List backward: ";
+    list.print_reverse();
+
+    cout << "Now deleting list item #5 using delete_pos() fucntion. " << endl;
+    list.delete_pos(5);
+    cout << "List forward: ";
+    list.print();
     cout << "List backward: ";
     list.print_reverse();
 
